9751af6abf76091fea11090599eb85961975b869
Blazhko Denis <disa2206@inbox.ru>
2024-04-30 11:27:41 +0500

    second commit

---
+++ checkers/checker.py
@@ -0,0 +1,15 @@
+from checkers.enums import CheckerType
+
+
+class Checker:
+    '''Отслеживает тип шашек'''
+    def __init__(self, type: CheckerType = CheckerType.NONE):
+        self.__type = type
+
+    @property
+    def type(self):
+        return self.__type
+
+    def change_type(self, type: CheckerType):
+        '''Изменение типа шашки'''
+        self.__type = type
